24ff85745483c62e879a3d5fa53c292a1ffa2a86
FrankSalazar71 <140365851+FrankSalazar71@users.noreply.github.com>
2023-11-07 09:12:18 -0500

    Primer ejercicio

---
+++ 35_FrankSalazar/src/main/java/com/mycompany/_franksalazar/animal_35.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany._franksalazar;
+
+/**
+ *
+ * @author Usuario24
+ */
+public class animal_35 {
+    
+    //atributos
+    
+    
+    
+}
---
+++ 35_FrankSalazar/src/main/java/com/mycompany/_franksalazar/estudiante_35.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany._franksalazar;
+
+/**
+ *
+ * @author Usuario24
+ */
+public class estudiante_35 {
+    
+}

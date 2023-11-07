50b65cf840e76d043b306facab0aba3e745adaf0
FrankSalazar71 <140365851+FrankSalazar71@users.noreply.github.com>
2023-11-07 10:18:31 -0500

    ejercicio numero 2

---
+++ 35_FrankSalazar2/src/main/java/com/mycompany/_franksalazar2/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._franksalazar2;
+
+/**
+ *
+ * @author Usuario24
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
